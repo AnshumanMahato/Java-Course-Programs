b6b1cae0aabd32c7fe5a3bb3e6a49952243bf939
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-05-25 11:06:44 +0530

    Print sum of 3 numbers

---
+++ programs/ThreeNumSum.java
@@ -0,0 +1,14 @@
+package programs;
+
+public class ThreeNumSum {
+    public static void main(String[] args)
+    {
+        int num1 = 7;
+        int num2 = 9;
+        int num3 = 12;
+        int sum = num1 + num2 + num3;
+
+        System.out.println("The sum of 3 numbers: " + sum);
+    }
+
+}
