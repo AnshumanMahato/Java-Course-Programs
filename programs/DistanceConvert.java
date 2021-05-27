3e03ddbfd002a34f219acde60316910cba2ae6e9
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-05-27 10:32:27 +0530

    Distance km to miles

---
+++ programs/DistanceConvert.java
@@ -0,0 +1,12 @@
+package programs;
+import java.util.Scanner;
+
+public class DistanceConvert {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Distance in kilometeres");
+        float km = sc.nextFloat();
+        System.out.println("Distance in Miles: " + (km*0.621371));
+        sc.close();
+    }
+}
