21306e6d45ae536ef0a6219c183bc7e878a951d9
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-07 11:16:35 +0530

    java practice set q2 done

--- programs/GradeEnc.java
+++ programs/GradeEnc.java
@@ -1,5 +1,20 @@
 package programs;
 
+import java.util.Scanner;
+
 public class GradeEnc {
-    
+    public static void main(String[] args) {
+        float marks;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter marks:");
+        marks = sc.nextFloat();
+        marks += 8;
+
+        System.out.println("Encrypted marks:" + marks);
+        System.out.println("Actual marks:" + (marks-8));
+
+        sc.close();
+
+    }
 }
