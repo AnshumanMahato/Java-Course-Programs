66080bdfad8811caa022deaf0f299abfe1e8ed73
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-07 11:23:44 +0530

    java practice set q3 done

--- programs/CompareNos.java
+++ programs/CompareNos.java
@@ -1,7 +1,24 @@
 package programs;
+import java.util.Scanner;
 
 public class CompareNos {
     public static void main(String[] args) {
-        
+        int num, setValue = 8;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a no.:");
+        num = sc.nextInt();
+
+        if(num < setValue) {
+            System.out.println("Entered no. is less than set value.");
+        }
+        else if(num > setValue) {
+            System.out.println("Entered no. is greater than set value.");
+        }
+        else {
+            System.out.println("Entered no. is equal to set value.");
+        }
+
+        sc.close();
     }
 }
