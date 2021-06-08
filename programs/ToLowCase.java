4153cbc4e4e2e6a827784ed931c2b221e794b568
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-08 11:51:38 +0530

    ch3 practice set p1 done

--- programs/ToLowCase.java
+++ programs/ToLowCase.java
@@ -1,7 +1,11 @@
 package programs;
+import java.util.Scanner;
 
 public class ToLowCase {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input.toLowerCase());
+        sc.close();
     }
 }
