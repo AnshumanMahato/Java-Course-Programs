e8fb5f9119bcff98121bb267a47630f4da3de5ad
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-08 11:55:12 +0530

    ch3 practice set p2 done

--- programs/SpaceToUnderscore.java
+++ programs/SpaceToUnderscore.java
@@ -1,8 +1,14 @@
 package programs;
+import java.util.Scanner;
 
 public class SpaceToUnderscore {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(input.replace(" ", "_"));
+
+        sc.close();
     }
     
 }
