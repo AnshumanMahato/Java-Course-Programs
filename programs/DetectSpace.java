8770993c86dee431e6bb7aa38b62baef7b480fe1
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-09 11:11:56 +0530

    ch3 practice 4 done

---
+++ programs/DetectSpace.java
@@ -0,0 +1,13 @@
+package programs;
+
+import java.util.Scanner;
+
+public class DetectSpace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println("is there any occurence of double or triple space:" + 
+                            (input.indexOf("  ") != -1 || input.indexOf("   ") != -1));
+        sc.close();
+    }
+}
