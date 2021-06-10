91d5cc5a05c939dd69f88cdebfb31bdb035106e7
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-10 11:23:15 +0530

    ch4 practice set 2

---
+++ programs/TestResult.java
@@ -0,0 +1,22 @@
+package programs;
+
+import java.util.Scanner;
+
+public class TestResult {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks[] = new int [3];
+        System.out.println("Enter marks for 3 subjects:");
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        int total = marks[0]+marks[1]+marks[2];
+
+        if(marks[0] < 33 && marks[1] < 33 && marks[3] < 33 && (total/3) < 40)
+            System.out.println("Fail");
+        else
+            System.out.println("pass");
+        sc.close();
+    }
+}
