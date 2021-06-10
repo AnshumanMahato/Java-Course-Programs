501954fde76f99326b3709602fd7f0a29c95e53f
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-10 11:31:50 +0530

    ch4 practice set 2 done

--- programs/TestResult.java
+++ programs/TestResult.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TestResult {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int marks[] = new int [3];
         System.out.println("Enter marks for 3 subjects:");
