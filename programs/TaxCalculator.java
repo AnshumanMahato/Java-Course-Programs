7518e8b578cd8f8d375045857e9a2013f6ad6c3d
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-10 11:45:57 +0530

    ch4 practice set 3 trail 1

--- programs/TaxCalculator.java
+++ programs/TaxCalculator.java
@@ -1,5 +1,23 @@
 package programs;
+import java.util.Scanner;
 
 public class TaxCalculator {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float income = sc.nextFloat();
+        float tax;
+
+        if(income < 250000.00)
+            tax = 0;
+        else if(income < 500000.00)
+            tax = 0.05f * income;
+        else if(income < 1000000.00)
+            tax = 0.2f * income;
+        else 
+            tax = 0.3f * income;
+
+        System.out.println("Tax : " + tax);
+
+        sc.close();
+    }
 }
