f9cfe595a8ab252bec87869a44854704bd3f6399
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-14 11:31:50 +0530

    java ch4 practice set 5 trail 1

--- programs/CH4/LeapYear.java
+++ programs/CH4/LeapYear.java
@@ -1,7 +1,25 @@
 package programs.CH4;
 
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        boolean isLeap = false;
+
+        if(year%100 == 0)
+        {
+            if(year%400 == 0)
+                isLeap = true;
+
+        }
+        else if(year%4 == 0)
+        {
+            isLeap = true;
+        }
         
+        System.out.println("Is" + year + " a leap year? " + isLeap);
+        sc.close();
     }
 }
