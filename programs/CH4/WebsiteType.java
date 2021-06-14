78d71f4eeb4ba52ccab4be6be9778644456c604f
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-14 11:44:22 +0530

    java ch4 practice set 6 trail 1

--- programs/CH4/WebsiteType.java
+++ programs/CH4/WebsiteType.java
@@ -1,7 +1,21 @@
 package programs.CH4;
 
+import java.util.Scanner;
+
 public class WebsiteType {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String url = sc.next();
+        String urlEnd = url.substring(url.lastIndexOf('.'));
+        String urlType = new String();
+
+        switch(urlEnd) {
+            case ".com": urlType = "Commercial Site";break;
+            case ".in": urlType = "Indian Site";break;
+            case ".org": urlType = "Organizational Site";break;
+        }
+
+        System.out.println("Site Type:" + urlType);
+        sc.close();
     }
 }
