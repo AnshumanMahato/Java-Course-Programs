ed386d627b316112975fa26da718f8e8242db1ef
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-15 11:18:11 +0530

    ch 5 practice set p8 done

--- programs/CH5/MultiplicationTableSum.java
+++ programs/CH5/MultiplicationTableSum.java
@@ -1,7 +1,18 @@
 package programs.CH5;
 
+import java.util.Scanner;
+
 public class MultiplicationTableSum {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        System.out.println("Table of " + n + ":");
+        for (int i = 1; i <= 10 ; i++) {
+            sum+= n*i;
+        }
+
+        System.out.println(sum);
+        sc.close();
     }
 }
