79acee79549769951b29b0313c11b63147300bad
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-15 11:14:05 +0530

    ch 5 practice set p4 done

--- programs/CH5/MultiplicationTableRev.java
+++ programs/CH5/MultiplicationTableRev.java
@@ -1,7 +1,18 @@
 package programs.CH5;
 
+import java.util.Scanner;
+
 public class MultiplicationTableRev {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("Table of " + n + ":");
+        int i = 10;
+        while(i > 0) {
+            System.out.println(n*i);
+            i--;
+        }
+        sc.close();
     }
 }
