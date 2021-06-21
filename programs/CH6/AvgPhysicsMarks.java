b45a181f3f4b26758efe452758c1a492a28fe7f1
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-21 11:11:16 +0530

    java array question 3 done

--- programs/CH6/AvgPhysicsMarks.java
+++ programs/CH6/AvgPhysicsMarks.java
@@ -2,6 +2,11 @@ package programs.CH6;
 
 public class AvgPhysicsMarks {
     public static void main(String[] args) {
-        
+        float phyMarks[] = {35f,100f,33f,66f,78f,91f,67f,88f,56f,83f};
+        float total = 0f;
+        for (float marks: phyMarks) {
+            total+= marks;
+        }
+        System.out.println("Avg marks:" + (total/phyMarks.length));
     }
 }
