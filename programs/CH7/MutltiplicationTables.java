63a7e52f0c6d4be736513183222a784158cf8232
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-25 10:55:15 +0530

    ch7 program 1 done

--- programs/CH7/MutltiplicationTables.java
+++ programs/CH7/MutltiplicationTables.java
@@ -2,6 +2,13 @@ package programs.CH7;
 
 public class MutltiplicationTables {
     public static void main(String[] args) {
-        
+        displayTables(7);
+    }
+
+    static void displayTables(int n) {
+        System.out.println("Table of " + n + ":");
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(n*i);
+        }
     }
 }
