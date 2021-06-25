bfd7c1aad21bdfa37e21e60eedbbee83cf897c43
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-25 10:59:59 +0530

    ch7 program 2 done

--- programs/CH7/Pattern.java
+++ programs/CH7/Pattern.java
@@ -2,6 +2,15 @@ package programs.CH7;
 
 public class Pattern {
     public static void main(String[] args) {
-        
+        displayPattern(10);
+    }
+
+    static void displayPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.print('\n');
+        }
     }
 }
