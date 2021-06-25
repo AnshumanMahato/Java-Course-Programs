1afd29d0ebe0a7bbcf199aaf56f56568a2a0617e
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-25 11:10:46 +0530

    ch7 program 3 done

--- programs/CH7/Fibonacci.java
+++ programs/CH7/Fibonacci.java
@@ -2,6 +2,14 @@ package programs.CH7;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getFibonacci(i));
+        }
+    }
+
+    static int getFibonacci(int n) {
+        if(n < 2)
+            return n;
+        return getFibonacci(n-1) + getFibonacci(n-2);
     }
 }
