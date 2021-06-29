aad300630d42d43def060fa5874245771e3c65fa
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-29 09:38:32 +0530

    ch7 practice p6 done

--- programs/CH7/Average.java
+++ programs/CH7/Average.java
@@ -2,6 +2,14 @@ package programs.CH7;
 
 public class Average {
     public static void main(String[] args) {
-        
+        System.out.println(avg(0,1,1,2,3,5));
+    }
+
+    static float avg(int ... nums) {
+        float avg = 0;
+        for (int i : nums) {
+            avg = avg + (float)i/(float)nums.length;
+        }
+        return avg;
     }
 }
