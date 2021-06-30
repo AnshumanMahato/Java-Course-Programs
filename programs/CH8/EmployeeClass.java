b3e21a896f3ac8cad493ceb562a91127416f3630
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-06-30 10:30:50 +0530

    Ch8 practice 1 done

--- programs/CH8/EmployeeClass.java
+++ programs/CH8/EmployeeClass.java
@@ -2,6 +2,32 @@ package programs.CH8;
 
 public class EmployeeClass {
     public static void main(String[] args) {
-        
+        Employee emp = new Employee();
+
+        emp.setName("Aman");
+        emp.setSalary(10000);
+
+        System.out.println("Name:" + emp.getName() + "\nSalary:" + emp.getSalary());
+    }
+}
+
+class Employee {
+    public int sal;
+    public String name;
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setSalary(int s) {
+        sal = s;
+    }
+
+    public int getSalary() {
+        return sal;
+    }
+
+    public String getName() {
+        return name;
     }
 }
