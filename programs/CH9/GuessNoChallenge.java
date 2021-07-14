bd3c809a6b4d3e3769c5be8aa995a32f931c55c4
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-14 11:40:13 +0530

    guess number challenge done

--- programs/CH9/GuessNoChallenge.java
+++ programs/CH9/GuessNoChallenge.java
@@ -1,7 +1,52 @@
 package programs.CH9;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessNoChallenge {
     public static void main(String[] args) {
+        SelectedNo gameNum = new SelectedNo();
+        gameNum.guessNo();
         
+    }
+}
+
+class SelectedNo {
+    private int num;
+    private int guessCount;
+
+    private boolean isCorrect(int n) {
+        return n == num;
+    }
+
+    public SelectedNo() {
+        Random rand = new Random();
+        num = rand.nextInt(100) + 1;
+        guessCount = 0;
+    }
+
+    public void guessNo(){
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        while(true) {
+            System.out.print("Enter Number:");
+            n = sc.nextInt();
+            guessCount++;
+            if(this.isCorrect(n)) {
+                System.out.println("Congratulation!!! you guessed it correct. You made " + guessCount + " guesses.");
+                break;
+            }
+            else if(n > num){
+                System.out.println("You're wrong. Guessed no. is greater than the actual no. Try Again!!!");
+            }
+            else{
+                System.out.println("You're wrong. Guessed no. is smaller than the actual no. Try Again!!!");
+            }
+        }
+
+        sc.close();
+            
     }
 }
