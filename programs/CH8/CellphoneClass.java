de044c4c3920ae3985bf80fecbd3b34c9588fb9c
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-14 10:19:16 +0530

    ch8 q2 done

--- programs/CH8/CellphoneClass.java
+++ programs/CH8/CellphoneClass.java
@@ -3,5 +3,20 @@ package programs.CH8;
 public class CellphoneClass {
     public static void main(String[] args) {
         
+        Cellphone phone = new Cellphone();
+
+        phone.ringing();
+        phone.vibrating();
+
+    }
+}
+
+class Cellphone {
+    public void ringing() {
+        System.out.println("Ringing");
+    }
+
+    public void vibrating() {
+        System.out.println("Vibrating");
     }
 }
