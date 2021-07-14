af6af401aa08840206ebd52a6e2db48b7b8a7bae
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-14 10:26:26 +0530

    ch8 q3 done

--- programs/CH8/SquareClass.java
+++ programs/CH8/SquareClass.java
@@ -2,6 +2,23 @@ package programs.CH8;
 
 public class SquareClass {
     public static void main(String[] args) {
-        
+        Square s1 = new Square();
+
+        s1.setSide(10);
+        System.out.println(s1.perimeter());
+        System.out.println(s1.area());
+    }
+}
+
+class Square {
+    private int side;
+    public void setSide(int s){
+        side = s;
+    }
+    public int perimeter() {
+        return side * 4;
+    }
+    public int area() {
+        return side*side;
     }
 }
