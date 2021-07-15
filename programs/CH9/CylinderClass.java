71a188a1d20b654e2d0bfe460d20af019f6c0544
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-15 11:05:30 +0530

    ch9 practice set q123 done

--- programs/CH9/CylinderClass.java
+++ programs/CH9/CylinderClass.java
@@ -2,6 +2,38 @@ package programs.CH9;
 
 public class CylinderClass {
     public static void main(String[] args) {
-        
+        Cylinder Cyl =  new Cylinder();
+        Cyl.setHeight(5);
+        Cyl.setRadius(10);
+        System.out.println(Cyl.getSurfaceArea());
+        System.out.println(Cyl.getVolume());
+    }
+}
+
+class Cylinder {
+    private int radius,height;
+
+    public Cylinder(){
+        radius = 0;
+        height = 0;
+    }
+
+    public Cylinder(int rad, int hei){
+        radius = rad;
+        height = hei;
+    }
+
+    public int getRadius(){return radius;}
+    public void setRadius(int r){radius = r;}
+
+    public int getHeight(){return height;}
+    public void setHeight(int h){height = h;}
+
+    public float getSurfaceArea(){
+        return (2 * 3.14f * radius * (radius + height));
+    }
+
+    public float getVolume(){
+        return 3.14f * radius * radius * height;
     }
 }
