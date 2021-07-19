7a878eb0180aed4b5a3ff8851677fa8869ee0785
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-19 11:08:31 +0530

    Book class created

--- programs/CH10/LibrarySystem.java
+++ programs/CH10/LibrarySystem.java
@@ -5,3 +5,33 @@ public class LibrarySystem {
         
     }
 }
+
+class Book {
+    private int bno;
+
+    public int getBno() {
+	    return this.bno;
+    }       
+    public void setBno(int bno) {
+	    this.bno = bno; 
+    }
+
+    private String name;
+
+    public String getName() {
+	    return this.name;
+    }   
+    public void setName(String name) {
+	    this.name = name;
+    }
+
+    public Book(String name, int bno)
+    {
+        this.name = name;
+        this.bno = bno;
+    }
+
+}
+
+
+
