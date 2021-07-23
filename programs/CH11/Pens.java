7aa26082d04239a66cfb58d61df9c44158d2939b
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-23 10:47:59 +0530

    Practice set 12 done

--- programs/CH11/Pens.java
+++ programs/CH11/Pens.java
@@ -2,6 +2,30 @@ package programs.CH11;
 
 public class Pens {
     public static void main(String[] args) {
-        
+        Pen p1 = new FountainPen();
+        FountainPen p2 = new FountainPen();
+
+        p1.write();
+        p1.refill();
+        p2.changeNib();
+    }
+}
+
+abstract class Pen {
+    abstract void write();
+    abstract void refill();
+}
+
+class FountainPen extends Pen {
+    void write(){
+        System.out.println("Writing...");
+    }
+
+    void refill(){
+        System.out.println("Refilling");
+    }
+
+    public void changeNib(){
+        System.out.println("Changing nibs");
     }
 }
