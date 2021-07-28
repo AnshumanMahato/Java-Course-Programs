4daa638c617f1698e280d503f5bb09d1d76bda3e
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-07-28 09:46:13 +0530

    ch12 practice set

---
+++ programs/CH12/HybridCalculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class HybridCalculator {
+    public static void main(String[] args) {
+        System.out.println("This is a hybrid calculator");
+    }
+}
---
+++ programs/CH12/ScCalculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class ScCalculator {
+    public static void main(String[] args) {
+        System.out.println("This is a Scientific calculator");
+    }
+}
